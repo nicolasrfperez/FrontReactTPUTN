7ff3ac8fae1d080ec0f77bea07df5aa1668195d5
Leandro Gil <leangilutn@gmail.com>
2020-11-24 21:50:27 -0300

    Ciclos de vida

--- src/App.js
+++ src/App.js
@@ -6,12 +6,44 @@ import Header from './Components/Header'
 import Menu from './Components/Menu'
 
 class App extends Component{
+  constructor(){
+    super()
+    //Define el state
+    this.state={
+      opciones:["Inicio","Login","Registro"],
+      titulo:"Hola Mundo"
+    }
+  }
+  handleClickTitle(){
+    this.setState({
+      titulo:"Chau Mundo"
+    })
+  }
+  handleClickLogin = ()=>{
+    //Modifica el valor de state
+    this.setState({
+      opciones:["Inicio","Salir"],
+    })
+  }
+  /*handleClickLogin(){
+
+  }*/
+  componentWillUpdate(nextProps,nextState){
+    console.log("componentWillUpdate App",nextProps,nextState)
+}
+
   render(){
-    const opciones = ["Inicio","Login","Registro"]
+
     return (
       <div className="App">
+        {this.state.titulo}
         <Home />
-        <Menu options={opciones} title="Hola Mundo"/>
+        <Menu options={this.state.opciones} click={this.handleClickLogin}>
+          <div>
+            <h1>{this.state.titulo}</h1>
+          </div>
+        </Menu>
+        <button onClick={this.handleClickTitle.bind(this)}>Cambiar Title</button>
         
       </div>
       
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,12 +1,39 @@
 import React,{Component} from "react";
 
+
+const styles={
+    div:{
+        backgroundColor:"red"
+    }
+    
+}
 class Menu extends Component{
+    componentWillMount(){
+        console.log("componentWillMount")
+    }
+    componentDidMount(){
+        console.log("componentDidMount")
+    }
+    componentWillReceiveProps(nextProps){
+        console.log("componentWillReceiveProps",nextProps)
+    }
+    componentWillUpdate(nextProps,nextState){
+        console.log("componentWillUpdate",nextProps,nextState)
+    }
+    componentDidUpdate(prevProps,prevState){
+        console.log("componentDidUpdate App",prevProps,prevState)
+      }
+      shouldComponentUpdate(nextProps, nextState){
+        console.log("shouldComponentUpdate",nextProps,nextState)
+        return false;
+      }
   render(){
     return (
       
-        <div>
-            {this.props.title}
+        <div style={styles.div}>
+            {this.props.children}
     {this.props.options.map(option=><li>{option}</li>)}
+            <button onClick={this.props.click}>Login</button>
         </div>
         
       
